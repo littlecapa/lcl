606f09994af2bea181e9fb82e692bbc8a348ec38
littlecapa <littlecapa@googlemail.com>
2024-11-28 09:45:38 +0100

    bug fixing singleton

--- lcl/__init__.py
+++ lcl/__init__.py
@@ -1,5 +1,5 @@
 from .template import add, sub
 from .net_lib import get_local_ip
-from singleton import SingletonMeta
+from .singleton import SingletonMeta
 
 __all__ = ["add", "sub", "get_local_ip", "SingletonMeta"]
